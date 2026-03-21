a53a20d20f69b54f6a458ff55281e13428c15117
431771129 <104458912+431771129@users.noreply.github.com>
2026-03-22 07:03:06 +0800

    Create messages.js

---
+++ ui/messages.js
@@ -0,0 +1,19 @@
+export function escapeHtml(str) {
+  if (!str) return '';
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/\//g, '&#x2F;');
+}
+
+export function getTypeText(type) {
+  const map = {
+    emby: '🎬 Emby',
+    domain: '🌐 域名',
+    sub: '📦 订阅'
+  };
+  return map[type] || type;
+}
